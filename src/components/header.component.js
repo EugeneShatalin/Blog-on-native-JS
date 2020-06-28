e77dd06a9b855903f46a624067ec49882f8b0e47
EugeneShatalin <shatalin_ei@mail.ru>
2020-06-28 18:49:10 +0600

    Добавил сохранение в localStorage информации об начале работы с приложением, для отключения повторного вывода приветственной страницы при обновлении страницы.

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -5,12 +5,16 @@ export class HeaderComponent extends Component {
         super(id) //обращаемся к конструктору родительского класса, для получения его методов
     }
 
-    init() {
+    init() { //вызываем метод родительского класса component
+        if(localStorage.getItem('visited')) {
+            this.hide()
+        }
        const btn = this.$el.querySelector('.js-header-start') //находим кнопку по классу
         btn.addEventListener('click', buttonHandler.bind(this)) //вызываем функцию при клике, добавляем bind, чтобы не терять контекст вызова
     }
 }
 
 function buttonHandler() {
-    this.hide()
+    localStorage.setItem('visited', JSON.stringify(true))
+    this.hide() //вызываем метод родительского класса component
 }
