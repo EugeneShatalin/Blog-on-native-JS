e24a4f16b4dd955d338cd4359ed482c2d702315d
EugeneShatalin <shatalin_ei@mail.ru>
2020-07-04 00:07:45 +0600

    Сделал валидацию полей формы.

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,5 +1,6 @@
 import {Component} from '../core/component'
 import {Form} from '../core/form'
+import {Validators} from '../core/validators'
 
 export class CreateComponent extends Component {
     constructor(id) {
@@ -10,21 +11,23 @@ export class CreateComponent extends Component {
         this.$el.addEventListener('submit', submitHandler.bind(this))
 
         this.form = new Form(this.$el, { //создаем класс form, передаем в него сам элемент формы и объект с валидаторами
-            title: [],
-            fulltext: []
+            title: [Validators.required],
+            fulltext: [Validators.required, Validators.minLength(10)]
         })
     }
-
-
 }
 
 function submitHandler(event) {
     event.preventDefault() //отменяем стандартное поведение (чтоб не перезагружалась форма)
 
-    const formData = { //собираем данные из формы в объект, реализовано двумя разными способами для поля с селектом и для полей без него
-        type: this.$el.type.value, //сохраняем значение из поля формы с селектом
-            ...this.form.value() //получаем остальные значения их формы
+    if(this.form.isValid()) {
+        const formData = { //собираем данные из формы в объект, реализовано двумя разными способами для поля с селектом и для полей без него
+            type: this.$el.type.value, //сохраняем значение из поля формы с селектом
+            ...this.form.value() //получаем остальные значения из формы
+        }
+        console.log('Submit', formData)
     }
-
-console.log('Submit', formData, this.form.value())
+    else {
+        console.warn('Form is invalid')
+    }
 }
--- src/core/form.js
+++ src/core/form.js
@@ -9,5 +9,23 @@ export class Form {
         Object.keys(this.controls).forEach(control => { //методом Object.keys берем ключи и с их помощью собираем данные из формы
             value[control] = this.form[control].value
         })
+        return value
+    }
+
+    isValid() {
+        let isFormValid = true
+
+        Object.keys(this.controls).forEach(control => {
+            const validators = this.controls[control]
+
+            let isValid = true
+            validators.forEach(validator => {
+                isValid = validator(this.form[control].value) && isValid
+            })
+
+            isFormValid = isFormValid && isValid
+        })
+
+        return isFormValid
     }
 }
