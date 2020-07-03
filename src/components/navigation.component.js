2831c619e815c1f40a9b4c1aaba340d3f4e41740
EugeneShatalin <shatalin_ei@mail.ru>
2020-07-03 20:02:48 +0600

    Добавил компоненты, отвечающие за разделы (табы) блога. Сделал рабочим переключение между разделами блога.

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -3,19 +3,29 @@ import {Component} from "../core/component";
 export class NavigationComponent extends Component {
     constructor(id) {
         super(id)
+
+        this.tabs = [] //масссив для сохранения компонент меню
     }
 
     init() {
         this.$el.addEventListener('click', tabClickHandler.bind(this))
+    }
+
+    registerTabs(tabs) {
+        this.tabs = tabs //метод для передаци в массив всех компонент меню
     }
 }
 
 function tabClickHandler(event) {
     event.preventDefault()  //отменяем у выбранного элемента все действия по умолчанию
     if (event.target.classList.contains('tab')) { //проверяем имеется ли у элемента класс tab
-        Array.from(this.$el.querySelector('tab')).forEach(tab => { //собираем все элементв с классом tab в массив
-            tab.classList.contains('active') //удаляем у все элементов класс active
+        Array.from(this.$el.querySelectorAll('.tab')).forEach(tab => { //собираем все элементв с классом tab в массив
+            tab.classList.remove('active') //удаляем у все элементов класс active
         })
         event.target.classList.add('active') //текущему элементу добавляем класс active
+
+        const activeTab = this.tabs.find(t => t.name === event.target.dataset.name) //
+        this.tabs.forEach(t => t.component.hide())
+        activeTab.component.show()
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import {HeaderComponent} from "./components/header.component"
 import {NavigationComponent} from "./components/navigation.component"
+import {PostsComponent} from "./components/posts.component.js" //компонента отвечающая за показ блока с id posts
+import {CreateComponent} from "./components/create.component.js" //компонента отвечающая за показ блока с id create
+import {FavoriteComponent} from "./components/favorite.component.js" //компонента отвечающая за показ блока с id favorite
 
 new HeaderComponent('header') //создаем компоненту header
 
 const navigation = new NavigationComponent('navigation') //создаем компоненту navigation
+
+//создаем переменные содержащие с себе компоненты для удобства дальнейшего использования
+const posts = new PostsComponent('posts')
+const create = new CreateComponent('create')
+const favorite = new FavoriteComponent('favorite')
+
+//передаем все три пункта меню (табы) в массив
+navigation.registerTabs([
+    {name: 'create', component: create},
+    {name: 'posts', component: posts},
+    {name: 'favorite', component: favorite}
+])
 
