94cb0e196b7636127eb46797674c3c49c881b796
EugeneShatalin <shatalin_ei@mail.ru>
2020-07-05 21:49:33 +0600

    Сделал сохранение id постов в localStorage, настроил кнопку сохранения - изменение вида в зависимости от сохранённых данных в localStorage.

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -8,6 +8,10 @@ export class PostsComponent extends Component {
         this.loader = loader
     }
 
+    init() {
+        this.$el.addEventListener('click', buttonHandler.bind(this))
+    }
+
     async onShow() {
         this.loader.show()
         const fbData = await apiService.fetchPosts() //получение данных с сервера
@@ -26,7 +30,11 @@ function renderPost(post) {
     const tag = post.type === 'news' //выводим тип поста в зависимости от типа
         ? `<li class="tag tag-blue tag-rounded">Новость</li>`
         : `<li class="tag tag-rounded">Заметка</li>`
-    const button = `<button class="button-round button-small button-primary">Сохранить</button>`
+
+    const button = (JSON.parse(localStorage.getItem('favorites')) || []).includes(post.id) //проверка на содержание в localStorage id текущего поста и в соответствии с этим вывод нужной кнопки
+        ? `<button class="button-round button-small button-danger" data-id="${post.id}">Удалит</button>`
+        : `<button class="button-round button-small button-primary" data-id="${post.id}">Сохранить</button>`
+
     return `
         <div class="panel">
           <div class="panel-head">
@@ -44,4 +52,27 @@ function renderPost(post) {
           </div>
         </div>
     `
+}
+
+function buttonHandler(event) { //обработка килка по кнопке
+    const $el = event.target //сохраняем в $el элемент по которому произошел клик
+    const id = $el.dataset.id //присваеваем id хранящийся в data-id элемента по которому произошел клик, если такой параметр у него имеется
+
+    if(id) {
+        let favorites = JSON.parse(localStorage.getItem('favorites')) || [] //берем данные из localStorage, если их нет, присваиваем пустой массив
+
+       if (favorites.includes(id)) { //если в массиве favorites нет текущего id
+           $el.textContent = 'Сохранить'
+           $el.classList.add('button-primary')
+           $el.classList.remove('button-danger')
+           favorites = favorites.filter(fid => fid != id)
+       } else { //если в массиве favorites есть текущий id
+           $el.classList.add('button-danger')
+           $el.classList.remove('button-primary')
+           $el.textContent = 'Удалить'
+           favorites.push(id)
+       }
+       localStorage.setItem('favorites', JSON.stringify(favorites)) //сохраняем в localStorage обновленные данные по id
+    }
+
 }
