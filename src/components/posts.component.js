a778016e519afa711463bd82338dc1f54578eeea
EugeneShatalin <shatalin_ei@mail.ru>
2020-07-05 20:49:29 +0600

    Сделал анимацию лоудинга при загрузке постов.

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -3,14 +3,45 @@ import {apiService} from '../services/api.service'
 import {TransformService} from "../services/transform.service";
 
 export class PostsComponent extends Component {
-    constructor(id) {
+    constructor(id, {loader}) {
         super(id)
+        this.loader = loader
     }
 
     async onShow() {
+        this.loader.show()
         const fbData = await apiService.fetchPosts() //получение данных с сервера
         const posts = TransformService.fbObjectToArray(fbData) //преобразование данных в массив
-        console.log(posts)
+        const html = posts.map(post => renderPost(post)) //выводим посты
+        this.loader.hide()
+        this.$el.insertAdjacentHTML('afterbegin', html.join(' '))
+    }
+
+    onHide() {
+        this.$el.innerHTML = '' //очищаем вкладку с постами при переключении на другую
     }
 }
 
+function renderPost(post) {
+    const tag = post.type === 'news' //выводим тип поста в зависимости от типа
+        ? `<li class="tag tag-blue tag-rounded">Новость</li>`
+        : `<li class="tag tag-rounded">Заметка</li>`
+    const button = `<button class="button-round button-small button-primary">Сохранить</button>`
+    return `
+        <div class="panel">
+          <div class="panel-head">
+            <p class="panel-title">${post.title}</p>
+            <ul class="tags">
+              ${tag}
+            </ul>
+          </div>
+          <div class="panel-body">
+            <p class="multi-line">${post.fulltext}</p>
+          </div>
+          <div class="panel-footer w-panel-footer">
+            <small>${post.date}</small>
+            ${button}
+          </div>
+        </div>
+    `
+}
--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import {HeaderComponent} from "./components/header.component"
 import {NavigationComponent} from "./components/navigation.component"
 import {PostsComponent} from "./components/posts.component.js" //компонента отвечающая за показ блока с id posts
 import {CreateComponent} from "./components/create.component.js" //компонента отвечающая за показ блока с id create
-import {FavoriteComponent} from "./components/favorite.component.js" //компонента отвечающая за показ блока с id favorite
+import {FavoriteComponent} from "./components/favorite.component.js"
+import {LoaderComponent} from "./components/loader.component"; //компонента отвечающая за показ блока с id favorite
 
 
 new HeaderComponent('header') //создаем компоненту header
 
 const navigation = new NavigationComponent('navigation') //создаем компоненту navigation
+const loader = new LoaderComponent('loader') //создаем компоненту loader
 
 //создаем переменные содержащие с себе компоненты для удобства дальнейшего использования
-const posts = new PostsComponent('posts')
+const posts = new PostsComponent('posts', {loader})
 const create = new CreateComponent('create')
 const favorite = new FavoriteComponent('favorite')
 
